edaf8410a80b5921df47aeabaaec7a17ffc83d58
Milind Choudhary <milindc02@gmail.com>
2024-01-30 21:39:04 -0600

    Deleted generation script
    Updating  file locations

--- scripts/qa_generation/run_t5.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python t5.py -t manual -m t5-3b
-CUDA_VISIBLE_DEVICES=0,1 python predict.py -x manual -y manual -m t5-3b
-CUDA_VISIBLE_DEVICES=0,1 python predict.py -x manual -y kairos_argument -m t5-3b
-
-CUDA_VISIBLE_DEVICES=0,1 python t5.py -t kairos_argument -m t5-3b
-CUDA_VISIBLE_DEVICES=0,1 python predict.py -x kairos_argument -y manual -m t5-3b
-CUDA_VISIBLE_DEVICES=0,1 python predict.py -x kairos_argument -y kairos_argument -m t5-3b
-
--- scripts/qa_generation/run_t5_combined.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=2,3 python t5.py -t manual-kairos_argument -m t5-large
-CUDA_VISIBLE_DEVICES=2,3 python predict.py -x manual-kairos_argument -y manual -m t5-large
-CUDA_VISIBLE_DEVICES=2,3 python predict.py -x manual-kairos_argument -y kairos_argument -m t5-large
-
