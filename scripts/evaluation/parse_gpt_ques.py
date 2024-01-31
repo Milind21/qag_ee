27ebd62671e9d787cd444c94960f6ba68fdc114e
Milind Choudhary <milindc02@gmail.com>
2024-01-30 21:46:42 -0600

    Update parse_gpt_ques.py
    Cleaned Up code

--- scripts/evaluation/parse_gpt_ques.py
+++ scripts/evaluation/parse_gpt_ques.py
@@ -7,33 +7,14 @@ for key,value in gold_data.items():
         for k,v in event.items():
             qa_pairs = v.split('\n')
             qa_list = []
-            #For QA format parse
-            # for qa_pair in qa_pairs:
-            #     try:
-            #         question, answer = qa_pair.split('\n')
-            #         question = question[3:]  # Remove 'Q: ' prefix
-            #         answer = answer[3:]      # Remove 'A: ' prefix
-            #         qa_dict = {'question': question, 'answer': answer}
-            #         qa_list.append(qa_dict)
-            #     except Exception as e:
-            #         continue
-            #For AQ format parse
             for i in range(0,len(qa_pairs),2):
                 try:
-                    # if(qa_pairs[i][0]=='Q'):
                     answer  = qa_pairs[i]
                     question  = qa_pairs[i+1]
                     question = question[3:]  # Remove 'Q: ' prefix
                     answer = answer[3:]      # Remove 'A: ' prefix
                     qa_dict = {'answer': answer,'question': question}
                     qa_list.append(qa_dict)
-                    # else:
-                    #     question   = qa_pairs[i+1]
-                    #     answer = qa_pairs[i]
-                    #     question = question[3:]  # Remove 'Q: ' prefix
-                    #     answer = answer[3:]      # Remove 'A: ' prefix
-                    #     qa_dict = { 'answer': answer, 'question': question}
-                    #     qa_list.append(qa_dict)
                 except Exception as e:
                     continue
             event[k]=qa_list
